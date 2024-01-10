a18afcf6026cbf13db9e8e77568d2807c715daa8
DusanDamjanov <105151463+DusanDamjanov@users.noreply.github.com>
2024-01-11 00:26:01 +0100

    Added role and refactored user model

--- app/src/main/java/com/example/bookit/retrofit/RetrofitService.java
+++ app/src/main/java/com/example/bookit/retrofit/RetrofitService.java
@@ -1,5 +1,26 @@
 package com.example.bookit.retrofit;
 
-public class RetrofitService {
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class RetrofitService {
+    private Retrofit retrofit;
+
+    public RetrofitService(){
+        initializeRetrofit();
+    }
+
+    private void initializeRetrofit() {
+        this.retrofit = new Retrofit.Builder()
+                .baseUrl("http://192.168.0.22:8080")
+                .addConverterFactory(GsonConverterFactory.create(new Gson()))
+                .build();
+    }
+
+    public Retrofit getRetrofit() {
+        return retrofit;
+    }
 }
