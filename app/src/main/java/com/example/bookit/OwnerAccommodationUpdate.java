ee11e6add2924858eb6ce210a5db1c3fa6e263c6
natasa-dz <dzudzar.n@gmail.com>
2024-01-11 18:14:57 +0100

    Added OwnerAccommodationView

---
+++ app/src/main/java/com/example/bookit/OwnerAccommodationUpdate.java
@@ -0,0 +1,15 @@
+package com.example.bookit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OwnerAccommodationUpdate extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_owner_accommodation_update);
+    }
+
+}
